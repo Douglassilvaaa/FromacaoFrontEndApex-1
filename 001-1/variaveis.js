e4f4b96d549855323866bf2d2da84d6295fc3d04
Windows <ipujicaned@gmail.comgit config --global user.email ipujicaned@gmail.comgit config --global user.name Douglas>
2024-03-23 13:27:23 -0300

    Aula 001- Js Basico

---
+++ 001-1/variaveis.js
@@ -0,0 +1,15 @@
+//Variaveis
+
+
+console.log ('Conceito e tipo de variaveis')
+//Variavel
+
+var nome = 'Elvis';
+var numero = 15;
+var verdade =true;
+var salario = 1978.99
+
+console.log(typeof nome);
+console.log(typeof numero);
+console.log(typeof verdade);
+console.log(typeof salario)
